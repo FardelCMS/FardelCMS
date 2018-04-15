644e410d69315d6499736fd57a848f8acd1ce6ae
Sepehr Hamzehlouy <s.hamzelooy@gmail.com>
2018-04-15 19:33:23 +0430

    a way to create admin user

--- manage.py
+++ manage.py
@@ -4,12 +4,25 @@ from flask_migrate import Migrate, MigrateCommand
 from web.app import create_app
 from web.ext import db
 
+from web.core.auth.models import User
+
 
 app = create_app(develop=True)
 migrate = Migrate(app, db)
 
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
+
+@manager.command
+def create_admin(email, password):
+	u = User(email, password)
+	db.session.add(u)
+	try:
+		db.session.commit()
+	except:
+		print("Error aquired, i think a user with this email already exists")
+		return
+	print("Successfull created an admin.")
 
 
 if __name__ == "__main__":
