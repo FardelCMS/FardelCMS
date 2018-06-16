2c9f117488d9f477c41c55fb5f61b44a61253c1f
Sepehr Hamzehlouy <s.hamzelooy@gmail.com>
2018-06-16 18:52:02 +0430

    change from flask-cache to flask-caching

--- fardel/ext.py
+++ fardel/ext.py
@@ -4,7 +4,7 @@ Required flask extensions objects in RatSnake are created here.
 
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
-from flask_cache import Cache
+from flask_caching import Cache
 from flask_login import LoginManager
 from flask_babel import Babel
 
