d8e9cb85b7075c8a9545425d064add0fb343917e
Sepehr Hamzehlouy <s.hamzelooy@gmail.com>
2018-08-03 13:41:19 +0430

    fix navbar direction

--- fardel/core/panel/template_tags.py
+++ fardel/core/panel/template_tags.py
@@ -4,6 +4,12 @@ from .sidebar import panel_sidebar
 def get_text_direction():
 	return current_app.config['PANEL_BASE_DIR']
 
+def rightrtl_leftltr():
+	return "right" if current_app.config['PANEL_BASE_DIR'] == "rtl" else "left"
+
+def leftrtl_rightltr():
+	return "left" if current_app.config['PANEL_BASE_DIR'] == "rtl" else "right"
+
 def get_sidebar():
 	return panel_sidebar.render()
 
@@ -13,4 +19,7 @@ def get_sitename():
 def add_globals(app):
 	app.jinja_env.globals['get_sitename'] = get_sitename
 	app.jinja_env.globals['get_sidebar'] = get_sidebar
+	
 	app.jinja_env.globals['get_text_direction'] = get_text_direction
+	app.jinja_env.globals['rightrtl_leftltr'] = rightrtl_leftltr
+	app.jinja_env.globals['leftrtl_rightltr'] = leftrtl_rightltr
